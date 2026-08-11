d8adcd0c2a83a9e799dbedc3820df6c38dcf8bde
tmshkr <5200096+tmshkr@users.noreply.github.com>
2026-08-11 10:01:10 -0700

    add igdb router

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,6 +6,7 @@ import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.ts";
 import { errorHandler } from "./lib/errors.ts";
 import { gamesRouter } from "./routes/games.ts";
+import { igdbRouter } from "./routes/igdb.ts";
 import { sessionsRouter } from "./routes/sessions.ts";
 import { requireAuth } from "./lib/requireAuth.ts";
 import { redirectToShelfIfSignedIn } from "./lib/pageAuth.ts";
@@ -69,6 +70,7 @@ app.use("/shelf", shelfRouter);
 
 app.use("/api/games", gamesRouter);
 app.use("/api/games/:gameId/sessions", sessionsRouter);
+app.use("/api/igdb", igdbRouter);
 
 app.use(errorHandler);
 
